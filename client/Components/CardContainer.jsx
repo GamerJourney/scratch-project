6892b901080df7e4ababd464ce5991e63b1be7ed
Andy <andy@Andys-MacBook-Air.local>
2022-04-26 11:07:19 -0400

    final

--- client/Components/CardContainer.jsx
+++ client/Components/CardContainer.jsx
@@ -1,15 +1,14 @@
 //We need CardContainer because the cards need to be presented seperately from the reset numbers etc in the header
-
 import React from 'react'
 import Card from './Card'
 import Styles from '../Styles/CardContainer.css'
+
 function CardContainer(props) {
 
 
 
     return (
         <div className="CardContainer">
-
             <div className="Cards">
                 {cardSet}
             </div>
